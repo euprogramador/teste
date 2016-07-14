da4a2ffa3e8d3ff65bb4bca5f43530ab711e3d5d
Carlos Alberto <euprogramador@gmail.com>
2016-07-14 08:59:34 -0300

    commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		int x=0;
+		while(true) {
+			System.out.println("imprimindo "+ ++x);
+			Thread.sleep(1000);
+		}
+	}
+	
+}
